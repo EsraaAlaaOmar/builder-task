f142ac1589576e712e7525b0cf1291584760a5c0
EsraaAlaaOmar <esraa.safa55@gmail.com>
2021-10-10 00:06:58 +0200

    static

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import faker from 'faker'
+ const Header= () =>{
+    return( <div className='header'>
+        <img src= 'images/logo.png' />
+         Es Builder                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+     </div>)
+ }
+ export default Header
